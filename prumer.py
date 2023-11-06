8dd981b991824e01e926f6ebd5529fccdead7b40
KarolinaGrymova <grymovak@arcig.cz>
2023-11-06 20:41:20 +0100

    oprava

--- prumer.py
+++ prumer.py
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import sys
-def prumer (seznam: List[int])->float:
-  souccet=0
-  for i in seznam
-    soucet=soucet+i
-  print(soucet/len(seznam))
-
-prumer(sys.argv[:2])
-
-#změna
-=======
 def prumer (seznam : list)-> float:
     suma=0
     for i in seznam:
@@ -18,4 +6,3 @@ def prumer (seznam : list)-> float:
     return vysledek 
 
 print(prumer([1,3,4,6]))
->>>>>>> 6929cac745ae391456b33087d484ed37f5cd532b
